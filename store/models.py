fce40959d6f96589a2b422dbd444a99993ca4c8b
Adams Ijachi <adamsijachi9@gmail.com>
2021-01-28 12:25:59 +0100

    created models and template links

--- store/models.py
+++ store/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.shortcuts import reverse
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+
+class Products(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    name = models.CharField(max_length=100)
+    image = models.ImageField(verbose_name='Product_Image',upload_to='img/products', null=True, blank=True)
+    price = models.FloatField()
+    updated_at = models.DateTimeField(auto_now=True,)
+    created_at = models.DateTimeField(auto_now_add=True)
+   
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('core:product_detail', kwargs={'pk':self.id})
+    
+    
+    
--- store/views.py
+++ store/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
+from .models import Products , Category
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'store/index.html')
+    products = Products.objects.all()
+    categories = Category.objects.all()
+    context = {'products':products,'categories':categories}
+    return render(request, 'store/index.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Products, pk=pk)
+    context = {'product':product}
+    return render(request, 'store/product-page.html', context)
+
+

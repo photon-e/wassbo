a432d441219c5145f4b8afb6ecccb3c852680cf2
Adams Ijachi <adamsijachi9@gmail.com>
2021-01-28 09:16:55 +0100

    initial commit

---
+++ accounts/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+# Create your models here.
+
+class CustommAccountManager(BaseUserManager):
+    def create_superuser(self, email, first_name, last_name, password, **other_fields):
+
+        other_fields.setdefault('is_staff',  True)
+        other_fields.setdefault('is_superuser',  True)
+        other_fields.setdefault('is_active',  True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff = True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_superuser = True.')
+        
+        return self.create_user(email, first_name, last_name, password, **other_fields)
+    
+    def create_user (self, email, first_name, last_name, password, **other_fields):
+
+        if not email:
+            raise ValueError(_('You must provide an email address'))
+
+        email = self.normalize_email(email)
+        user = self.model( email=email, first_name = first_name,last_name=last_name, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    username = models.CharField(max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    start_date = models.DateTimeField(default=timezone.now)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_customer = models.BooleanField(default=False)
+    is_agent = models.BooleanField(default=False)
+
+    objects = CustommAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name','last_name']
+
+    def __str__(self):
+        return self.email

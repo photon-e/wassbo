9a40a579f2ca915be800154199b8faf173a4c1a3
Ezeokafor <gootech442@gmail.com>
2021-01-29 22:35:24 +0100

    dev

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,4 @@
-from django.db import models
 
-# Create your models here.
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
--- store/views.py
+++ store/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'store/index.html')
+    return render(request, 'store/index.html', {'home_view': home_view})
